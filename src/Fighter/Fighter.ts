c15ff53edc017854cd3c01a2c70af6e3f2eb909e
Danrllei Miranda <danmiranda14@gmail.com>
2024-01-28 18:45:52 -0300

    requisito 10

--- src/Fighter/Fighter.ts
+++ src/Fighter/Fighter.ts
@@ -1,11 +1,12 @@
 import Energy from '../Energy';
+import SimpleFighter from './SimpleFighter';
 
 export default interface Fighter {
   lifePoints: number;
   strength: number;
   defense: number;
   energy?: Energy;
-  attack(enemy: Fighter | import('./Fighter').default): void;
+  attack(enemy: SimpleFighter | import('./Fighter').default): void;
   special?(enemy: Fighter): void;
   levelUp(): void;
   receiveDamage(attackPoints: number): number;
